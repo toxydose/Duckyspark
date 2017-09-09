d57aade80fa89f64926c7069a5498d4bd05fcf01
Eclipse <eclipse@awake.pro>
2017-09-09 17:13:44 +0300

    add support of special buttons, some code refactor

--- Duckyspark_translator.py
+++ Duckyspark_translator.py
@@ -10,168 +10,112 @@
 from __future__ import print_function
 import sys
 
+# order is important
+special_buttons = { 'CONTROL_RIGHT' : 'MOD_CONTROL_RIGHT',
+                    'CONTROL_LEFT'  : 'MOD_CONTROL_LEFT',
+                    'CONTROL'       : 'MOD_CONTROL_LEFT',
+
+                    'CTRL_RIGHT'    : 'MOD_CONTROL_RIGHT',
+                    'CTRL_LEFT'     : 'MOD_CONTROL_LEFT',
+                    'CTRL'          : 'MOD_CONTROL_LEFT',
+
+                    'SHIFT_RIGHT'   : 'MOD_SHIFT_RIGHT',
+                    'SHIFT_LEFT'    : 'MOD_SHIFT_LEFT',
+                    'SHIFT'         : 'MOD_SHIFT_LEFT',
+
+                    'ALT_RIGHT'     : 'MOD_ALT_RIGHT',
+                    'ALT_LEFT'      : 'MOD_ALT_LEFT',
+                    'ALT'           : 'MOD_ALT_LEFT',
+
+                    'GUI_RIGHT'     : 'MOD_GUI_RIGHT',
+                    'GUI_LEFT'      : 'MOD_GUI_LEFT',
+                    'GUI'           : 'MOD_GUI_LEFT',
+                    'WINDOWS'       : 'MOD_GUI_LEFT' }
+
+# menu = Shift + F10
+
+buttons = { 'a' : 'KEY_A',
+            'b' : 'KEY_B',
+            'c' : 'KEY_С',
+            'd' : 'KEY_D',
+            'e' : 'KEY_E',
+            'f' : 'KEY_F',
+            'g' : 'KEY_G',
+            'h' : 'KEY_H',
+            'i' : 'KEY_I',
+            'j' : 'KEY_J',
+            'k' : 'KEY_K',
+            'l' : 'KEY_L',
+            'm' : 'KEY_M',
+            'n' : 'KEY_N',
+            'o' : 'KEY_O',
+            'p' : 'KEY_P',
+            'q' : 'KEY_Q',
+            'r' : 'KEY_R',
+            's' : 'KEY_S',
+            't' : 'KEY_T',
+            'u' : 'KEY_U',
+            'v' : 'KEY_V',
+            'w' : 'KEY_W',
+            'x' : 'KEY_X',
+            'y' : 'KEY_Y',
+            'z' : 'KEY_Z',
+
+            'F1' : 'KEY_F1',
+            'F2' : 'KEY_F2',
+            'F3' : 'KEY_F3',
+            'F4' : 'KEY_F4',
+            'F5' : 'KEY_F5',
+            'F6' : 'KEY_F6',
+            'F7' : 'KEY_F7',
+            'F8' : 'KEY_F8',
+            'F9' : 'KEY_F9',
+            'F10' : 'KEY_F10',
+            'F11' : 'KEY_F11',
+            'F12' : 'KEY_F12',
+
+            'LEFTARROW' : 'KEY_ARROW_LEFT',
+            'RIGHTARROW' : 'KEY_ARROW_RIGHT',
+            'UPARROW' : 'KEY_ARROW_UP',
+            'DOWNARROW' : 'KEY_ARROW_DOWN',
+            'LEFT' : 'KEY_ARROW_LEFT',
+            'RIGH' : 'KEY_ARROW_RIGHT',
+            'UP' : 'KEY_ARROW_UP',
+            'DOWN' : 'KEY_ARROW_DOWN',
+
+            'DELETE' : 'KEY_DELETE',
+            'DEL' : 'KEY_DELETE',
+            'PRINTSCREEN' : 'KEY_PRT_SCR',
+            'TAB' : 'KEY_TAB',
+            'ESCAPE' : 'KEY_ESC',
+            'SPACE' : 'KEY_SPACE',
+            'ENTER' : 'KEY_ENTER' }
+
 payload_input = ''
-l='//'
-mod_input=''
-mod_output=''
-
-def replacement():
-	print ('DigiKeyboard.', end ='')
-	print(
-str(l.replace(' a', 'KEY_A').replace(' a ', 'KEY_A')
-.replace(' b', 'KEY_B')
-.replace(' c', 'KEY_C')
-.replace(' d', 'KEY_D')
-.replace(' e', 'KEY_E')
-.replace(' f', 'KEY_F')
-.replace(' g', 'KEY_G')
-.replace(' h', 'KEY_H')
-.replace(' i', 'KEY_I')
-.replace(' j', 'KEY_J')
-.replace(' k', 'KEY_K')
-.replace(' l', 'KEY_L')
-.replace(' m', 'KEY_M')
-.replace(' n', 'KEY_N')
-.replace(' o', 'KEY_O')
-.replace(' p', 'KEY_P')
-.replace(' q', 'KEY_Q')
-.replace(' r', 'KEY_R')
-.replace(' s', 'KEY_S')
-.replace(' t', 'KEY_T')
-.replace(' u', 'KEY_U')
-.replace(' v', 'KEY_V')
-.replace(' w', 'KEY_W')
-.replace(' x', 'KEY_X')
-.replace(' y', 'KEY_Y')
-.replace(' z', 'KEY_Z')
-
-#1-0 if needed
-
-#f1-f12
-.replace(' F1','KEY_F1')
-.replace(' F2','KEY_F2')
-.replace(' F3','KEY_F3')
-.replace(' F4','KEY_F4')
-.replace(' F5','KEY_F5')
-.replace(' F6','KEY_F6')
-.replace(' F7','KEY_F7')
-.replace(' F8','KEY_F8')
-.replace(' F9','KEY_F9')
-.replace(' F10','KEY_F10')
-.replace(' F11','KEY_F11')
-.replace(' F12','KEY_F12')
-#arrows 
-.replace('LEFTARROW', 'KEY_ARROW_LEFT')
-.replace('RIGHTARROW', 'KEY_ARROW_RIGHT')
-.replace('UPARROW','KEY_ARROW_UP')
-.replace('DOWNARROW','KEY_ARROW_DOWN')
-.replace('LEFT', 'KEY_ARROW_LEFT')
-.replace('RIGH', 'KEY_ARROW_RIGHT')
-.replace('UP','KEY_ARROW_UP')
-.replace('DOWN','KEY_ARROW_DOWN')
-#keys
-.replace('PRINTSCREEN','sendKeyStroke(KEY_PRT_SCR' )
-.replace('TAB', 'sendKeyStroke(KEY_TAB')
-.replace('SPACE', 'sendKeyStroke(KEY_SPACE')
-.replace('CONTROL ALT','sendKeyStroke(MOD_ALT_RIGHT,')
-.replace('CTRL ALT','sendKeyStroke(MOD_ALT_RIGHT,')
-.replace('ESCAPE','sendKeyStroke(KEY_ESC' )
-.replace('ENTER','sendKeyStroke(KEY_ENTER')),end = '')
-	print(');')
-
-def modreplacement():
-	print ('DigiKeyboard.', end ='')
-	print('sendKeyStroke(', end = '')
-
-	print(
-str(l.replace (mod_input, '').replace(' a', 'KEY_A').replace(' a ', 'KEY_A')
-.replace(' b', 'KEY_B')
-.replace(' c', 'KEY_С')
-.replace(' d', 'KEY_D')
-.replace(' e', 'KEY_E')
-.replace(' f', 'KEY_F')
-.replace(' g', 'KEY_G')
-.replace(' h', 'KEY_H')
-.replace(' i', 'KEY_I')
-.replace(' j', 'KEY_J')
-.replace(' k', 'KEY_K')
-.replace(' l', 'KEY_L')
-.replace(' m', 'KEY_M')
-.replace(' n', 'KEY_N')
-.replace(' o', 'KEY_O')
-.replace(' p', 'KEY_P')
-.replace(' q', 'KEY_Q')
-.replace(' r', 'KEY_R')
-.replace(' s', 'KEY_S')
-.replace(' t', 'KEY_T')
-.replace(' u', 'KEY_U')
-.replace(' v', 'KEY_V')
-.replace(' w', 'KEY_W')
-.replace(' x', 'KEY_X')
-.replace(' y', 'KEY_Y')
-.replace(' z', 'KEY_Z')
-
-#1-0
-
-#f1-f12
-.replace(' F1','KEY_F1')
-.replace(' F2','KEY_F2')
-.replace(' F3','KEY_F3')
-.replace(' F4','KEY_F4')
-.replace(' F5','KEY_F5')
-.replace(' F6','KEY_F6')
-.replace(' F7','KEY_F7')
-.replace(' F8','KEY_F8')
-.replace(' F9','KEY_F9')
-.replace(' F10','KEY_F10')
-.replace(' F11','KEY_F11')
-.replace(' F12','KEY_F12')
-
-#arrows 
-.replace('LEFTARROW', 'KEY_ARROW_LEFT')
-.replace('RIGHTARROW', 'KEY_ARROW_RIGHT')
-.replace('UPARROW','KEY_ARROW_UP')
-.replace('DOWNARROW','KEY_ARROW_DOWN')
-.replace('LEFT', 'KEY_ARROW_LEFT')
-.replace('RIGH', 'KEY_ARROW_RIGHT')
-.replace('UP','KEY_ARROW_UP')
-.replace('DOWN','KEY_ARROW_DOWN')
-
-.replace('PRINTSCREEN','sendKeyStroke(KEY_PRT_SCR' )
-.replace('TAB', 'sendKeyStroke(KEY_TAB')
-.replace('ESCAPE','KEY_ESC' )
-.replace('SPACE', 'KEY_SPACE')
-.replace(' ','')
-.replace('ENTER','KEY_ENTER')),end = '')
-
-	print(','+mod_output, end = '')				
-	print(');')
-
-
 #arguments
 if len(sys.argv) == 2:
-	try:
-		payload_input = open(sys.argv[1], "r")
-		sys.stdout = open("digipayload.ino", "w")
-		z = len(open(sys.argv[1], "r").readlines())
-	except IOError:
-		print('\nError! File "'+sys.argv[1]+'" does not exist!\n' )
-		exit()
+    try:
+        payload_input = open(sys.argv[1], "r")
+        sys.stdout = open("digipayload.ino", "w")
+        z = len(open(sys.argv[1], "r").readlines())
+    except IOError:
+        print('\nError! File "'+sys.argv[1]+'" does not exist!\n' )
+        exit()
 elif len(sys.argv) == 3:
-	try:
-		payload_input = open(sys.argv[1], "r")
-		sys.stdout = open(sys.argv[2]+'.ino', 'w')
-		z = len(open(sys.argv[1], "r").readlines())
-	except IOError:
-		print('\nError!, File "'+sys.argv[1]+'" does not exist!\n' )
-		exit()
+    try:
+        payload_input = open(sys.argv[1], "r")
+        sys.stdout = open(sys.argv[2]+'.ino', 'w')
+        z = len(open(sys.argv[1], "r").readlines())
+    except IOError:
+        print('\nError!, File "'+sys.argv[1]+'" does not exist!\n' )
+        exit()
 elif len(sys.argv) > 3:
-	print('Too much Arguments')
-	exit()
-else:	
-	payload_input = open('payload.txt', "r")
-	sys.stdout = open("digipayload.ino", "w")
-	z = len(open('payload.txt', "r").readlines())
+    print('Too much Arguments')
+    exit()
+else:    
+    payload_input = open('payload.txt', "r")
+    sys.stdout = open("digipayload.ino", "w")
+    z = len(open('payload.txt', "r").readlines())
 
 #--------------------------------------
 #Digispark program fragment
@@ -181,7 +125,10 @@ print('#define KEY_ESC     41')
 print('#define KEY_BACKSPACE 42')
 print('#define KEY_TAB     43')
 print('#define KEY_PRT_SCR 70')
-print('#define KEY_DELETE  76\n')
+print('#define KEY_DELETE  76')
+print('#define KEY_ARROW_RIGHT 0x4F')
+print('#define KEY_ARROW_DOWN  0x51')
+print('#define KEY_ARROW_UP    0x52\n')
 
 print('void setup() {\n')
 print('DigiKeyboard.delay(5000);') #windows mozhet dolgo raspoznavat digispark potomu bylo resheno dobavlyat 5 sek delay vmesto 0.5sek
@@ -189,65 +136,53 @@ print('DigiKeyboard.sendKeyStroke(0);')
 #---------------------------------------
 
 for i in range(z):
-	l = payload_input.readline().replace('\n', '')
-	
-	if len (l) < 1:
-		print('', end = '')
+    l = payload_input.readline().replace('\n', '')
 
-	else:
+    if len (l) < 1:
+        print('', end = '')
 
-		if 'REM' in l:
-			print ('//', l.replace('REM ', ''))	
-		
-		else:
-			if 'DELAY' in l:
-				print ('DigiKeyboard.', end = '')
-				print (l.replace('DELAY', 'delay(').replace(' ',''), end = '')
-				print(');')
-						
-			elif 'STRING' in l:
-				print ('DigiKeyboard.', end = '')
-				print (l.replace('"', '")); DigiKeyboard.print(char(34)); DigiKeyboard.print(F("')
-				       .replace('\\', '")); DigiKeyboard.print(char(92)); DigiKeyboard.print(F("')
-				       .replace('STRING ','print(F("'), end = '')
-				print ('")', end = '')
-				print(');');
-				
-			elif (l == 'GUI') or (l == 'WINDOWS') or (l == 'CONTROL ESCAPE'):
-				print('DigiKeyboard.sendKeyStroke(KEY_ESC,MOD_CONTROL_LEFT);')
+    else:
 
-			elif (l == 'GUI d') or (l == 'WINDOWS d'):
-				print ('DigiKeyboard.sendKeyStroke(KEY_D,MOD_GUI_LEFT);')
-			
-			elif (l == 'WINDOWS r') or (l == 'GUI r'):
-				print ('DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);')
+        if 'REM' in l:
+            print ('//', l.replace('REM ', ''))    
+        
+        else:
+            if 'DELAY' in l:
+                print ('DigiKeyboard.', end = '')
+                print (l.replace('DELAY', 'delay(').replace(' ',''), end = '')
+                print(');')
+                        
+            elif 'STRING' in l:
+                print ('DigiKeyboard.', end = '')
+                print (l.replace('"', '")); DigiKeyboard.print(char(34)); DigiKeyboard.print(F("')
+                       .replace('\\', '")); DigiKeyboard.print(char(92)); DigiKeyboard.print(F("')
+                       .replace('STRING ','print(F("'), end = '')
+                print ('")', end = '')
+                print(');')
 
-			elif 'MENU' in l:
-				print ('DigiKeyboard.sendKeyStroke(MOD_GUI_RIGHT);')
-				
-		#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!INVERCE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!	
-			elif 'CTRL ALT' in l:
-				replacement()
-			elif 'ALT' in l:
-				mod_input = 'ALT'
-				mod_output = 'MOD_ALT_RIGHT'
-				modreplacement()
-			elif 'CTRL' in l:
-				mod_input = 'CTRL'
-				mod_output = 'MOD_CONTROL_LEFT'
-				modreplacement()
+            else:
+                mod = ''
+                for key in special_buttons.keys():
+                    if key in l: # order in keys is important
+                        l = l.replace(key, '')
+                        mod += special_buttons.get(key) + ' | '
+                mod += '0'
 
-			elif 'CONTROL' in l:
-				mod_input = 'CONTROL'
-				mod_output = 'MOD_CONTROL_LEFT'
-				modreplacement()
+                l = l.replace(' ', '')
+                if l in buttons.keys():
+                    l = buttons.get(l)
+                else:
+                    l = '0'
 
-		#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!		
-			else:
-				replacement()
+                print('DigiKeyboard.', end='')
+                print('sendKeyStroke(', end='')
+                print(str(l) , end='')
+                print(',' + mod, end='')
+                print(');')
 
-		if len(l) <1:
-			print('', end = '')
+        if len(l) < 1:
+            print('', end = '')
+
 #Digispark program fragment
 print('\n}')
 print('\n')
